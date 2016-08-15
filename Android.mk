29e1b714994953eef81c76166a99072dce0818e8
zhuxyo <zhuxyo@gmail.com>
2016-08-15 10:12:41 +0800

    fix

--- Android.mk
+++ Android.mk
@@ -1,9 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_INCLUDES := $(LOCAL_PATH)
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_SRC_FILES := linux.c linux_termios.c serialport.c
+
+LOCAL_CFLAGS += -D__linux__ -D__ANDROID__
 
 LOCAL_MODULE:= libserialport
 
